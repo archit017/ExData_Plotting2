303b2a3af304fd1e02aadd5a4882e86233f94b53
Sergey Storchay <r8@r8.com.ua>
2014-09-22 01:07:02 +0300

    Implement plot1

--- plot1.R
+++ plot1.R
@@ -1,5 +1,15 @@
 url <- 'http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip'
 
-download.file(url, 'data/exdata-data-NEI_data.zip')
-unzip('data/exdata-data-NEI_data.zip', exdir='./data')
+if (!file.exists('data/exdata-data-NEI_data.zip')) {
+  download.file(url, 'data/exdata-data-NEI_data.zip')
+  unzip('data/exdata-data-NEI_data.zip', exdir='./data')
+}
 
+pmEmissionsData <- readRDS('data/summarySCC_PM25.rds')
+sourceClassificationCode <- readRDS('data/Source_Classification_Code.rds')
+
+data <- aggregate(Emissions ~ year, data=pmEmissionsData, sum)
+
+png('plot1.png', width = 480, height = 480)                                                                              
+plot(data$year, data$Emissions, type="b", main="Total Emissions Per Year", xlab="Year", ylab="Total Emissions")
+dev.off()
